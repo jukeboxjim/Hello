e46ad21b181422be5d8b1f2ff03f1276ed7e7e9a
jukeboxjim <jukeboxjim@users.noreply.github.com>
2016-08-01 19:38:14 +0100

    slow down
    change sleep times

--- P1_led_1.py
+++ P1_led_1.py
@@ -3,10 +3,10 @@ test 1
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
+led = LED(7)
 
 while True:
     led.on()
-    sleep(1)
+    sleep(5)
     led.off()
-    sleep(1)
+    sleep(5)
